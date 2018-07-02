b19b0c6b3c23064af3ad9ee41d1fc0a104ea7c6a
Ezreal Xie <ezrealxie@yahoo.com>
2018-07-03 06:07:13 +0800

    Update Jul_03_2018 06:07
    Webpages added

--- src/com/brynhild/ezrealxie/webinfomanagesys/UserLogin.java
+++ src/com/brynhild/ezrealxie/webinfomanagesys/UserLogin.java
@@ -2,7 +2,6 @@ package com.brynhild.ezrealxie.webinfomanagesys;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,27 +9,79 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.mail.handlers.text_html;
+/**
+ * Servlet implementation class UserLogin
+ */
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-public class UserLogin extends HttpServlet{
-	
+    
+    public boolean validateCredential(String inputUserNameString, String inputPasswordString) {
+		
+		Connection dbConnection = null;
+		PreparedStatement pStatement = null;
+		ResultSet rSet = null;
+		
+		//Acquire database driver
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver acquired.");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Driver missing.");
+		}
+		
+		//Establish database connection.
+		
+		try {
+			dbConnection = DriverManager.getConnection("jdbc:mysql://rm-uf6t232h92unv3jwgto.mysql.rds.aliyuncs.com/default_schema", "webuser_login", "login");
+			if (dbConnection != null) {
+				System.out.println("Connection established.");
+			} else {
+				System.out.println("Database connection error.");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		try {
+			String sqlString = "SELECT * FROM user_group WHERE username = ? + and password = ?";
+			pStatement = dbConnection.prepareStatement(sqlString);
+			pStatement.setString(1, inputUserNameString);
+			pStatement.setString(2, inputPasswordString);
+			rSet = pStatement.executeQuery();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		if (rSet != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+    
 	/**
-	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	private static final long serialVersionUID = -6373168450250335169L;
-
-	public UserLogin(){
-		super();
-	}
-	public void destroy(){
-		super.destroy();
-	}
-
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html;charset=utf-8");
 		response.setCharacterEncoding("utf-8");
@@ -69,10 +120,33 @@ public class UserLogin extends HttpServlet{
 		//Validate credential
 		
 		try {
-			String sqlString = "SELECT * FROM user_group WHERE username = " + inputUserNameString + "and password = " + inputPasswordString;
+			String sqlString = "SELECT uid FROM user_group WHERE username = ? + and password = ?";
+			pStatement = dbConnection.prepareStatement(sqlString);
+			pStatement.setString(1, inputUserNameString);
+			pStatement.setString(2, inputPasswordString);
+			rSet = pStatement.executeQuery();
 		} catch (Exception e) {
 			// TODO: handle exception
+			e.printStackTrace();
 		}
 		
+		if (rSet != null) {
+			pWOut.println("Log In.");
+		} else {
+			pWOut.println("Failed.");
+		}
+		
+		pWOut.flush();
+		pWOut.close();
+
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
